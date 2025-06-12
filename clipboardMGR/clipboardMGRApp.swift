d0c55c77544a5d19add33bc4c62aaf199347d1b7
F1Justin <f1justin@outlook.com>
2025-06-12 08:32:06 +0800

    Initial Commit

---
+++ clipboardMGR/clipboardMGRApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipboardMGRApp.swift
+//  clipboardMGR
+//
+//  Created by Justin on 2025/6/12.
+//
+
+import SwiftUI
+
+@main
+struct clipboardMGRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clipboardMGRTests/clipboardMGRTests.swift
@@ -0,0 +1,17 @@
+//
+//  clipboardMGRTests.swift
+//  clipboardMGRTests
+//
+//  Created by Justin on 2025/6/12.
+//
+
+import Testing
+@testable import clipboardMGR
+
+struct clipboardMGRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
